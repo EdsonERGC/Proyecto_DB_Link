cebde068d861e2e85411c066be0e7113a1f2c24f
julietapinto <78383832+julietapinto@users.noreply.github.com>
2025-10-29 23:24:38 -0600

    Conexion a Oracle
    Configuración para acceder a base de datos desde python

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask, jsonify
+import oracledb
+import mysql.connector
+from mysql.connector import Error
+from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-@app.route('/api/mensaje')
-def mensaje():
-    return jsonify({"mensaje": "Hola desde Flask 🚀"})
+def ConectionOracle():
+    host = "192.168.184.130"
+    port = 1521
+    service_name = "XEPDB1"
+    user = "PROYECTO"
+    password = "proyecto"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    try:
+        conn = oracledb.connect(
+            user=user,
+            password=password,
+            dsn=f"{host}:{port}/{service_name}"
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM PERSONAS FETCH FIRST 1 ROWS ONLY")
+        result = cursor.fetchone()
+        return f"Conexión exitosa a Oracle. Primer registro: {result}"
+
+    except oracledb.Error as e:
+        return f"Error al conectar a Oracle: {e}"
+
+    finally:
+        if 'conn' in locals() and conn:
+            cursor.close()
+            conn.close()
+
 
 #comentario
+
 
